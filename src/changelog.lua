25fa121d0cf122ecbf9adf57e142257ade28c6e3
Seniru Pasan Indira <senirupasan@gmail.com>
2020-11-27 07:30:42 +0000

    chore: Add new maps

--- src/changelog.lua
+++ src/changelog.lua
@@ -1,8 +1,12 @@
-local VERSION = "v2.2.2.2"
+local VERSION = "v2.2.2.3"
 local CHANGELOG =
 [[
 
 <p align='center'><font size='20'><b><V>CHANGELOG</V></b></font> <BV><a href='event:log'>[View all]</a></BV></p><font size='12' face='Lucide Console'>
+
+    <font size='15' face='Lucida Console'><b><BV>v2.2.2.3</BV></b></font> <i>(11/27/2020)</i>
+        • Added new maps
+
 
     <font size='15' face='Lucida Console'><b><BV>v2.2.2.2</BV></b></font> <i>(11/15/2020)</i>
         • Halloween sale ends!
