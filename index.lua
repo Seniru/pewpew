f8328020494ef2408b3f84ac0fd04a88ba8ac48b
GitHub Action <action@github.com>
2021-02-13 12:36:52 +0000

    ci: Automated build

--- index.lua
+++ index.lua
@@ -722,12 +722,36 @@ translations["es"] = {
 	SOLE =          "<ROSE>¡${player} es el único superviviente!"
 }
 
-translations["fr"] = {        
-	LIVES_LEFT =    "<ROSE>Il te reste <N>${lives} <ROSE>vies. <VI>Réapparition dans 3...",        
-	LOST_ALL =      "<ROSE>Tu as perdu toutes tes vies !",        
-	SD =            "<VP>Mort subite ! Il ne reste plus qu'<N>1 <VP>vie à tout le monde",        
-	WELCOME =       "<VP>Bienvenue sur pewpew, <N>baisse toi <VP>ou utilise <N>la barre d'espace <VP>pour tirer des objets !",        
-	SOLE =          "<ROSE>${player} est le seul survivant !"
+translations["fr"] = {	
+	LIVES_LEFT = "<ROSE>Tu as encore <N>${lives} <ROSE>vies restantes. <VI>Réapparition dans 3...",	
+	LOST_ALL =	"<ROSE>Tu as perdu toutes tes vies !",	
+	SD =		"<VP>Mort soudaine ! Tout le monde a <N>1 <VP>seule vie restante",	
+	WELCOME =	"<VP>Bienvenue dans pewpew, <N>baisse-toi <VP>ou utilise la <N>barre d'espace <VP>pour tirer des objets !",	
+    SOLE =		"<ROSE>${player} est le dernier survivant !",
+    SURVIVORS = "<ROSE>${winners} et ${winner} ont survécu à cette manche !",
+    SELF_RANK = "<p align='center'>Ton rang : ${rank}</p>",
+    ROUNDS  =   "<font face='Lucida console'><N2>Manches jouées</N2></font>",
+    DEATHS =    "<font face='Lucida console'><N2>Morts</N2></font>",
+    SURVIVED =  "<font face='Lucida console'><N2>Manches survécues</N2></font>",
+    WON =       "<font face='Lucida console'><N2>Manches gagnées</N2></font>",
+    LBOARD_POS = "<b><font face='Lucida console' color='#e3b134'>Classement global : ${pos}</font></b>",
+    EQUIPPED =  "Equipé",
+    EQUIP =     "Utiliser",
+    BUY =       "Acheter",
+    POINTS =    "<font face='Lucida console' size='12'>   <b>Points :</b> <V>${points}</V></font>",
+    PACK_DESC = "\n\n<font face='Lucida console' size='12' color='#cccccc'><i>“ ${desc} ”</i></font>\n<p align='right'><font size='10'>- ${author}</font></p>",
+    GIFT_RECV = "<N>Tu as été récompensé avec un(e) <ROSE><b>${gift}</b></ROSE> de la part de <ROSE><b>${admin}</b></ROSE>",
+    COMMANDS =  "\n\n<N2>[ <b>H</b> ]</N2> <N><ROSE>!help</ROSE> (Affiche le menu d'aide)</N><br><N2>[ <b>P</b> ]</N2> <N><ROSE>!profile <i>[joueur]</i></ROSE> (Affiche le profile d'un joueur)</N><br></N><N2>[ <b>O</b> ]</N2> <N><ROSE>!shop</ROSE> (Ouvre le magasin)</N><br><N2>[ <b>L</b> ]</N2> <N>(Affiche le classement)</N><br><br><N><ROSE>!changelog</ROSE> (Affiche l'historique des changements)</N><br><br>",
+    CMD_TITLE = "<font size='25' face='Comic Sans'><b><J>Commandes</J></b></font>",
+    CREDITS =   "\n\nArtiste - <b><BV>Lightymouse</BV><G>#0421</G></b>\nTraducteurs - <b><BV>Overforyou</BV><G>#9290</G>, <BV>Nuttysquirrel</BV><G>#0626</G>, <BV>Star</BV><G>#6725</G>, <BV>Jaker</BV><G>#9310</G></b>\n\n\nEt merci de jouer à Pewpew!",
+    CREDS_TITLE = "<font size='25' face='Comic Sans'><b><R>Credits</R></b></font>",
+    OBJECTIVE = "<b>Survie et tue les autres pour gagner.</b>",
+    HELP_GOTIT = "<font size='15'><J><b><a href='event:close'>Compris !</a></b></J></font>",
+    HELP_GITHUB = "<N>Envie de contribuer à ce module ? Cool ! Va sur </N> <VI><b><i>https://github.com/Seniru/pewpew</i></b></VI>",
+    HELP_DISCORD = "<N>Discord : </N> <VI><b><i>https://discord.gg/vaqgrgp</i></b></VI>",
+    HELP_MAP = "<N>Tu voudrais voir tes maps dans pewpew ? Va sur </N> <VI><b><i>https://atelier801.com/topic?f=6&t=892550</i></b></VI>",
+    NEW_ROLE = "<N><ROSE><b>${player}</b></ROSE> est maintenant un(e) <ROSE><b>${role}</b></ROSE>",
+    KICK_ROLE = "<N><ROSE><b>${player}</b></ROSE> n'est plus un(e) <ROSE><b>${role}</b></ROSE> ! ;c",
 }
 
 translations["tr"] = {        
