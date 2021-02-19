6c297bd428f0afd496fb2c48044a9cb885aaaff2
Seniru Pasan Indira <senirupasan@gmail.com>
2021-02-19 05:28:28 +0000

    experiment: I'm going to swear soon

--- buildtools/discord-webhooks.js
+++ buildtools/discord-webhooks.js
@@ -3,6 +3,8 @@ const { Webhook, MessageBuilder } = require('discord-webhook-node');
 
 const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET || process.argv[1]
 
+console.log("test", WEBHOOK_SECRET, process.env.WEBHOOK_SECRET, process.argv)
+
 const hook = new Webhook(`https://discord.com/api/webhooks/${WEBHOOK_SECRET}`);
 
 let main = async () => {
